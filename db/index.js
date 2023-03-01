c9332e32bf08a53e9d927d6f496ae9472a130484
Alexandra <alexmiriamd@gmail.com>
2023-03-01 10:04:05 -0700

    write some helper funcs started, not connected to seed.js yet

--- db/index.js
+++ db/index.js
@@ -2,6 +2,16 @@ const { Client } = require('pg');
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
+async function getAllUsers(){       //helper func from "Write Some Helper Functions" not connected to seed.js yet
+    const { rows } = await client.query(
+        `SELECT id, username
+        FROM users;
+    `);
+
+    return rows;
+}
+
 module.exports ={
     client,
+    getAllUsers,
 }
