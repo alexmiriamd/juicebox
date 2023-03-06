4c6c87a2f47b1977db0c72b02b0e111aec8bbf04
dylanstewart06 <dylanstewart06@gmail.com>
2023-03-06 09:59:10 -0700

    Writing POST completed

--- api/posts.js
+++ api/posts.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const postsRouter = express.Router();
-
+const {createPost} = require('../db');
 const {getAllPosts} = require('../db');
 
 const { requireUser } = require('./utils')
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
-    res.send({ message: 'under construction' });
+    const { title, content, tags = ""} = req.body;
+    const authorId = req.user.id
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = {
+        authorId,
+        title,
+        content
+    };
+
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+
+    try{
+        const post = await createPost (postData)
+        if (post) {
+        res.send( post )}
+        
+        else {
+            next ({name: "Post creation error", message: "Error creating post"})
+        }
+    } catch ({ name, message}) {
+        next({ name, message})
+    }
 });
 
 postsRouter.use((req, res, next) => {
