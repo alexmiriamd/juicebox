1c985c70f7ceae9f7db418ae223ada23d099cc4a
Alexandra <alexmiriamd@gmail.com>
2023-03-06 13:07:43 -0700

    ready for deployment

--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tagsRouter = express.Router();
 
-const {getAllTags} = require('../db');
+const {getAllTags, getPostsByTagName} = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
@@ -16,5 +16,28 @@ tagsRouter.get('/', async (req, res) => {
         tags
     });
 });
+
+tagsRouter.get('/:tagname/posts', async (req, res, next) => {       // get posts with tag (GET)
+    const tagName  = req.params.tagname;
+    try{
+        const allPosts = await getPostsByTagName(tagName);
+
+        const posts = allPosts.filter(post => {
+
+            if(!post.active){
+                return false
+            }
+            if(req.user && post.author.id !== req.user.id){
+                return false
+            }
+            return true
+        })
+
+        res.send({ posts });
+
+    } catch({ name, message }) {
+        next({name, message});
+    }
+})
 
 module.exports = tagsRouter;
--- db/index.js
+++ db/index.js
@@ -191,6 +191,13 @@ async function getPostById(postId) {
             FROM posts
             WHERE id=$1;
         `, [postId]);
+        
+        if(!post) {
+            throw{
+                name: "PostNotFoundError",
+                message: "Could not find a post with that postId"
+            };
+        }
 
         const { rows: [ tags ] } = await client.query(`
             SELECT tags.*
@@ -307,6 +314,7 @@ module.exports ={
     updateUser,
     createPost,
     getAllPosts,
+    getPostById,
     updatePost,
     getUserById,
     createTags,
